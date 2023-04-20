24003206faf9a7c626e08c66775b31bf9d827622
shipmadison <shipmadison@gmail.com>
2023-04-20 12:09:13 -0700

    Team 2 Structure

---
+++ Lab-3/Team-2/MHS_explr.R
@@ -0,0 +1,12 @@
+
+# load MARSS for data and analyses
+library(MARSS)
+
+## load the raw data (there are 3 datasets contained here)
+data(lakeWAplankton, package = "MARSS")
+
+## we want `lakeWAplanktonTrans`, which has been transformed
+## so the 0's are replaced with NA's and the data z-scored
+all_dat <- lakeWAplanktonTrans
+
+
