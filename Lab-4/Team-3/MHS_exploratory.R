3d28a106bc403154f519aaa8df596c86cd1aa621
shipmadison <shipmadison@gmail.com>
2023-04-27 14:48:36 -0700

    Create MHS_exploratory.R

---
+++ Lab-4/Team-3/MHS_exploratory.R
@@ -0,0 +1,39 @@
+stoplight <- read.csv(here::here("Lab-4", "stoplight.csv"))
+
+head(stoplight)
+names(stoplight)
+
+## add some code here
+class(stoplight)
+names(stoplight)
+
+#ecosystem indicators
+stoplight[,1]  
+years<-seq(1998,2021,1)
+
+#ecosystem indicators to pull
+PDO_DecMarch<-unlist(as.vector(stoplight[1,3:26])) 
+SST<-unlist(as.vector(stoplight[4,3:26])) 
+Copepod_richness<-unlist(as.vector(stoplight[9,3:26])) 
+Ichthy_community_index<-unlist(as.vector(stoplight[14,3:26]))
+
+dat<-matrix(,nrow=length(years), ncol=5)
+dat[,1]<-years
+dat[,2]<-PDO_DecMarch
+dat[,3]<-SST
+dat[,4]<-Copepod_richness
+dat[,5]<-Ichthy_community_index
+colnames(dat)<-c("years","PDO", "SST", "CopeRich", "FishInx")
+
+#five times series in response and keep things to your block 
+
+#Lecture example
+set.seed(123)
+mod = depmix(list(L_stilbius ~ 1, L_ochotensis~1), 
+             nstates = 2, 
+             family = list(gaussian(),gaussian()),
+             data=calcofi)
+fitmod = fit(mod)
+
+
+#pick some plankton, pick some drivers, 
