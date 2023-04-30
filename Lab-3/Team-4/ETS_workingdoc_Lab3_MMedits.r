8a0df4881c03727cd66959bcb0af66e3fca46443
Emma Timmins-Schiffman <emmats@uw.edu>
2023-04-29 20:30:26 -0700

    draft of final document for team 4

--- Lab-3/Team-4/ETS_workingdoc_Lab3_MMedits.r
+++ Lab-3/Team-4/ETS_workingdoc_Lab3_MMedits.r
@@ -66,19 +66,19 @@ Z<-matrix(Z.vals, nrow=N.ts, ncol=3, byrow=T)
 #Q and B are equal to identity matrix
 Q<-B<-diag(1,3) # "identity"
 
-#assume same observation variance for R matrix
+#assume different observation variance for R matrix
 R<-"diagonal and unequal"
 
 x0<-U<-A<-"zero" # also init_list <- list(x0 = matrix(rep(0,m),m,1)) where m = # processes
 V0<-diag(5,3)
 
 dfa.model1<-list(Z=Z, A="zero", R=R, B=B, U=U, Q=Q, x0=x0, V0=V0)
-cntl.list<-list(maxit=100) #increase this later to 1000
+cntl.list<-list(maxit=100) #may want to set this higher since error that convergence was not reached
 
 #fit the model
 dfa.fit1<-MARSS(plank.t, model=dfa.model1, control=cntl.list)
 
-autoplot(dfa.fit3)
+autoplot(dfa.fit1, plot.type='acf.std.model.resids.ytt1')
 #ACF shows quite a bit of autocorrelation - this needs to be addressed
 
 #compare AICc for 2, 3, or 4 underlying trends
@@ -111,7 +111,7 @@ trends.rot = solve(H_inv) %*% dfa.fit3$states
 
 #####################################################
 #####################################################
-
+w_ts <- seq(dim(plank.t)[2])
 mm <- 4
 ## plot the processes
 for(i in 1:mm) {
@@ -192,7 +192,7 @@ mod_fit <- getDFAfits(dfa.fit3)
 # plot the fits
 
 ylbl <- rownames(plank.t)
-w_ts <- seq(dim(plank.t)[2])
+#w_ts <- seq(dim(plank.t)[2])
 par(mfcol = c(5, 1), mar = c(3, 4, 1.5, 0.5), oma = c(0.4, 1, 1, 1))
 for (i in 1:N.ts) {
   up <- mod_fit$up[i, ]
@@ -268,8 +268,17 @@ dfa_seas <- MARSS(plank.t, model = mod_list, form = "dfa", z.score = FALSE,
 dfa_TPseas <- MARSS(plank.t, model = mod_list, form = "dfa", z.score = FALSE,
                   control=cntl.list, covariates = rbind(TP, dd))
 
+dfa_TPtempseas <- MARSS(plank.t, model = mod_list, form = "dfa", z.score = FALSE,
+                    control=cntl.list, covariates = rbind(temp,TP, dd))
+
+dfa_tempseas <- MARSS(plank.t, model = mod_list, form = "dfa", z.score = FALSE,
+                        control=cntl.list, covariates = rbind(temp, dd))
+
+#AICc for TP + seasons and TP + temp + seasons are similar and lowest
+
 ## get model fits & CI's
 mod_fit <- getDFAfits(dfa_TPseas)
+mod_fit2 <- getDFAfits(dfa_TPtempseas)
 
 ## plot the fits
 par(mfrow = c(N.ts, 1), mai = c(0.5, 0.7, 0.1, 0.1), omi = c(0, 0, 0, 0))
@@ -288,18 +297,243 @@ for(i in 1:N.ts) {
   lines(w_ts, lo, col = "darkgray")
 }
 
-print(cbind(model = c("no covars", "Temp", "TP", "Temp & TP", "Seasonality", "TP Seas"),
+for(i in 1:N.ts) {
+  up <- mod_fit2$up[i,]
+  mn <- mod_fit2$ex[i,]
+  lo <- mod_fit2$lo[i,]
+  plot(w_ts, mn, type = "n",
+       xlab = "", ylab = ylbl[i],
+       xaxt = "n", cex.lab = 1.2,
+       ylim = c(min(lo), max(up)))
+  axis(1, 12 * (0:dim(plank.t)[2]) + 1, yr_frst + 0:dim(plank.t)[2])
+  points(w_ts, plank.t[i,], pch = 16, col = clr[i])
+  lines(w_ts, up, col = "darkgray")
+  lines(w_ts, mn, col = "black", lwd = 2)
+  lines(w_ts, lo, col = "darkgray")
+}
+
+
+#plots of both fits appear visually the same
+
+print(cbind(model = c("no covars", "Temp", "TP", "Temp & TP", "Seasonality", "TP Seas", "TP & temp & Seas", "temp & Seas"),
             AICc = round(c(dfa.fit3$AICc,
                            dfa_temp$AICc,
                            dfa_TP$AICc,
                            dfa_both$AICc,
                            dfa_seas$AICc,
-                           dfa_TPseas$AICc))),
+                           dfa_TPseas$AICc,
+                           dfa_TPtempseas$AICc,
+                           dfa_tempseas$AICc))),
       quote = FALSE)
 
 # It looks like TP and seasonality together are the best fit model.
 
-# Results and Discusssion
+#####################
+#The first 10 years of the data seem to have a stronger seasonality. Does the model fit better to just the first 10 years?
+all_dat2 <- lakeWAplanktonRaw
+plank.10yr<-subset(all_dat2, select=c("Year", 'Unicells', 'Other.algae', 'Cyclops', 'Diaptomus', 'Non.colonial.rotifers'), all_dat[,1]<1973)
+
+#transpose data
+plank.t10<-t(plank.10yr)
+year.cols<-plank.t10[1,]
+plank.t10<-plank.t10[2:6,]
+colnames(plank.t10)<-year.cols
+N.ts<-nrow(plank.t10)
+TT<-ncol(plank.t10)
+# re-do z-score
+dat.z<-zscore(plank.t10)
+
+temp.10yr <- t(all_dat2[1:132,"Temp", drop = FALSE])
+TP.10yr <- t(all_dat2[1:132,"TP", drop = FALSE])
+
+temp.z<-zscore(temp.10yr)
+TP.z<-zscore(TP.10yr)
+
+mod_list = list(m = 4, R = "diagonal and unequal")
+
+cos_t <- cos(2 * pi * seq(TT) / 12)
+sin_t <- sin(2 * pi * seq(TT) / 12)
+dd.10 <- rbind(cos_t, sin_t)
+
+#dfa_TPseas.10 <- MARSS(plank.t10, model = mod_list, form = "dfa", z.score = TRUE,
+ #                control=cntl.tmp, covariates = rbind(TP.z,dd.10))
+#Error: Stopped in MARSS.dfa() due to specification problem(s).
+#I cannot figure out why the TP does not work here
+
+dfa_tempseas.10 <- MARSS(plank.t10, model = mod_list, form = "dfa", z.score = TRUE,
+                        control=cntl.list, covariates = rbind(temp.z, dd.10))
+
+mod_fit.tempseas.10 <- getDFAfits(dfa_tempseas.10)
+
+autoplot(dfa_tempseas.10)
+
+w_ts_10 <- seq(dim(plank.t10)[2])
+
+par(mfrow = c(N.ts, 1), mai = c(0.5, 0.7, 0.1, 0.1), omi = c(0, 0, 0, 0))
+for(i in 1:N.ts) {
+  up <- mod_fit.tempseas.10$up[i,]
+  mn <- mod_fit.tempseas.10$ex[i,]
+  lo <- mod_fit.tempseas.10$lo[i,]
+  plot(w_ts_10, mn, type = "n",
+       xlab = "", ylab = ylbl[i],
+       xaxt = "n", cex.lab = 1.2,
+       ylim = c(min(lo), max(up)))
+  axis(1, 12 * (0:dim(plank.t10)[2]) + 1, yr_frst + 0:dim(plank.t10)[2])
+  points(w_ts_10, dat.z[i,], pch = 16, col = clr[i])
+  lines(w_ts_10, up, col = "darkgray")
+  lines(w_ts_10, mn, col = "black", lwd = 2)
+  lines(w_ts_10, lo, col = "darkgray")
+}
+
+Z_est.10 <- coef(dfa_tempseas.10, type = "matrix")$Z
+H_inv.10 <- varimax(Z_est.10)$rotmat
+
+# rotate factor loadings
+Z.rot.10 = Z_est.10 %*% H_inv.10
+trends.rot.10 = solve(H_inv.10) %*% dfa_tempseas.10$states
+
+m10 <- dim(trends.rot.10)[1]
+ylims <- c(-1.1 * max(abs(Z.rot.10)), 1.1 * max(abs(Z.rot.10)))
+par(mfrow = c(ceiling(m10 / 2), 2), mar = c(3, 4, 1.5, 0.5), oma = c(0.4, 1, 1, 1))
+for (i in 1:m10) {
+  plot(c(1:N.ts)[abs(Z.rot.10[, i]) > minZ], as.vector(Z.rot.10[abs(Z.rot.10[, i]) > minZ, i]),
+       type = "h", lwd = 2, xlab = "", ylab = "", xaxt = "n", ylim = ylims, xlim = c(0, N.ts + 1)
+  )
+  for (j in 1:N.ts) {
+    if (Z.rot.10[j, i] > minZ) {
+      text(j, -0.05, spp[j], srt = 90, adj = 1, cex = 0.9)
+    }
+    if (Z.rot.10[j, i] < -minZ) {
+      text(j, 0.05, spp[j], srt = 90, adj = 0, cex = 0.9)
+    }
+    abline(h = 0, lwd = 1, col = clr[i])
+  } # end j loop
+  mtext(paste("Factor loadings on trend", i, sep = " "), side = 3, line = .5)
+}
+
+for(i in 1:mm) {
+  ylm <- c(-1, 1) * max(abs(trends.rot.10[i,]))
+  ## set up plot area
+  plot(w_ts_10,trends.rot.10[i,], type = "n", bty = "L",
+       ylim = ylm, xlab = "", ylab = "", xaxt = "n")
+  ## draw zero-line
+  abline(h = 0, col = "gray")
+  ## plot trend line
+  lines(w_ts_10, trends.rot.10[i,], lwd = 2)
+  lines(w_ts_10, trends.rot.10[i,], lwd = 2)
+  ## add panel labels
+  mtext(paste("State",i), side = 3, line = 0.5)
+  axis(1, 12 * (0:dim(plank.t10)[2]) + 1, yr_frst + 0:dim(plank.t10)[2])
+}
+
+#OR!!! Try this after the big TP dump into the lake - does this change effect of TP in the model?
+all_dat2 <- lakeWAplanktonRaw
+plot.ts(all_dat2[,4])
+TP.sub<-subset(all_dat2, select=c("Year", "TP"), all_dat[,1]<1991)
+TP.sub<-subset(all_dat2, select=c("Year", "TP"), all_dat[,1]>1979)
+plot(x=TP.sub[,1], y=TP.sub[,2])
+plank.10yr<-subset(all_dat2, select=c("Year", 'Unicells', 'Other.algae', 'Cyclops', 'Diaptomus', 'Non.colonial.rotifers'), all_dat[,1]<1991)
+plank.10yr<-subset(all_dat2, select=c("Year", 'Unicells', 'Other.algae', 'Cyclops', 'Diaptomus', 'Non.colonial.rotifers'), all_dat[,1]>1979)
+
+
+#transpose data
+plank.t10<-t(plank.10yr)
+year.cols<-plank.t10[1,]
+plank.t10<-plank.t10[2:6,]
+colnames(plank.t10)<-year.cols
+N.ts<-nrow(plank.t10)
+TT<-ncol(plank.t10)
+# re-do z-score
+dat.z<-zscore(plank.t10)
+
+temp.10yr <- t(all_dat2[1:180,"Temp", drop = FALSE])
+TP.10yr <- t(all_dat2[1:180,"TP", drop = T])
+
+temp.z<-zscore(temp.10yr)
+TP.z<-zscore(TP.10yr)
+#replace NA with 0 in TP.z
+TP.z[is.na(TP.z)] <- 0
+
+mod_list = list(m = 3, R = "diagonal and unequal")
+
+dfa_TPseas.10 <- MARSS(plank.t10, model = mod_list, form = "dfa", z.score = TRUE,
+              control=cntl.tmp, covariates = rbind(TP.z,dd.10))
+
+
+dfa_temp.10 <- MARSS(plank.t10, model = mod_list, form = "dfa", z.score = TRUE,
+                         control=cntl.tmp, covariates = rbind(temp.z))
+
+mod_fit.temp.10 <- getDFAfits(dfa_temp.10)
+mod_fit.TPseas.10 <- getDFAfits(dfa_TPseas.10)
+#Error in ZZ %*% H_inv : non-conformable arguments
+#this may be because of missing values?
+
+autoplot(dfa_temp.10)
+autoplot(dfa_TPseas.10)
+
+w_ts_10 <- seq(dim(plank.t10)[2])
+
+par(mfrow = c(N.ts, 1), mai = c(0.5, 0.7, 0.1, 0.1), omi = c(0, 0, 0, 0))
+for(i in 1:N.ts) {
+  up <- mod_fit.TPseas.10$up[i,]
+  mn <- mod_fit.TPseas.10$ex[i,]
+  lo <- mod_fit.TPseas.10$lo[i,]
+  plot(w_ts_10, mn, type = "n",
+       xlab = "", ylab = ylbl[i],
+       xaxt = "n", cex.lab = 1.2,
+       ylim = c(min(lo), max(up)))
+  axis(1, 12 * (0:dim(plank.t10)[2]) + 1, yr_frst + 0:dim(plank.t10)[2])
+  points(w_ts_10, dat.z[i,], pch = 16, col = clr[i])
+  lines(w_ts_10, up, col = "darkgray")
+  lines(w_ts_10, mn, col = "black", lwd = 2)
+  lines(w_ts_10, lo, col = "darkgray")
+}
+
+#doesn't look like a great fit
+
+Z_est.10 <- coef(dfa_TPseas.10, type = "matrix")$Z
+H_inv.10 <- varimax(Z_est.10)$rotmat
+
+# rotate factor loadings
+Z.rot.10 = Z_est.10 %*% H_inv.10
+trends.rot.10 = solve(H_inv.10) %*% dfa_temp.10$states
+
+m10 <- dim(trends.rot.10)[1]
+ylims <- c(-1.1 * max(abs(Z.rot.10)), 1.1 * max(abs(Z.rot.10)))
+par(mfrow = c(ceiling(m10 / 2), 2), mar = c(3, 4, 1.5, 0.5), oma = c(0.4, 1, 1, 1))
+for (i in 1:m10) {
+  plot(c(1:N.ts)[abs(Z.rot.10[, i]) > minZ], as.vector(Z.rot.10[abs(Z.rot.10[, i]) > minZ, i]),
+       type = "h", lwd = 2, xlab = "", ylab = "", xaxt = "n", ylim = ylims, xlim = c(0, N.ts + 1)
+  )
+  for (j in 1:N.ts) {
+    if (Z.rot.10[j, i] > minZ) {
+      text(j, -0.05, spp[j], srt = 90, adj = 1, cex = 0.9)
+    }
+    if (Z.rot.10[j, i] < -minZ) {
+      text(j, 0.05, spp[j], srt = 90, adj = 0, cex = 0.9)
+    }
+    abline(h = 0, lwd = 1, col = clr[i])
+  } # end j loop
+  mtext(paste("Factor loadings on trend", i, sep = " "), side = 3, line = .5)
+}
+
+mm<-3
+for(i in 1:mm) {
+  ylm <- c(-1, 1) * max(abs(trends.rot.10[i,]))
+  ## set up plot area
+  plot(w_ts_10,trends.rot.10[i,], type = "n", bty = "L",
+       ylim = ylm, xlab = "", ylab = "", xaxt = "n")
+  ## draw zero-line
+  abline(h = 0, col = "gray")
+  ## plot trend line
+  lines(w_ts_10, trends.rot.10[i,], lwd = 2)
+  lines(w_ts_10, trends.rot.10[i,], lwd = 2)
+  ## add panel labels
+  mtext(paste("State",i), side = 3, line = 0.5)
+  axis(1, 12 * (0:dim(plank.t10)[2]) + 1, yr_frst + 0:dim(plank.t10)[2])
+}
+
+# Results and Discussion
 
 # We first determined the optimal number of states by fitting multiple models, 
 # each with different numbers of states. The models were then compared via AICc to determine 
