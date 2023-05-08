060614aa2e68aa49b2d95bc4a758da76ed3693b5
shipmadison <shipmadison@gmail.com>
2023-05-08 11:54:46 -0700

    Lab 4 updates

--- Lab-4/Team-3/MHS_exploratory.R
+++ Lab-4/Team-3/MHS_exploratory.R
@@ -43,10 +43,22 @@ ggplot(dat, aes(x = years)) +
   geom_line(aes(y = PDO, color = "PDO"), size = 1) +
   geom_line(aes(y = SST, color = "SST"), size = 1) +
   geom_line(aes(y = CopeRich, color = "CopeRich"), size = 1) +
-  #geom_line(aes(y = FishInx, color = "FishInx"), size = 1) +
+  geom_line(aes(y = FishInx, color = "FishInx"), size = 1) +
   scale_color_manual(values = c(PDO = "blue", SST = "red", CopeRich = "green", FishInx = "purple")) +
   labs(x = "Year", y = "Value", color = "Variable") +
   theme_minimal()
+
+# PDO states 
+ggplot(dat, aes(x = years)) +
+  geom_line(aes(y = PDO, color = "PDO"), size = 1) +
+  scale_color_manual(values = c(PDO = "blue", SST = "red", CopeRich = "green", FishInx = "purple")) +
+  labs(x = "Year", y = "Value", color = "Variable") +
+  geom_hline(yintercept = 0, linetype = "dotted") +
+  geom_hline(yintercept = 2.5, linetype = "dotted") +
+  geom_hline(yintercept = -2.5, linetype = "dotted") +
+  theme_minimal()
+
+#Think of weak vs strong PDO?
 
 #CopeRichness tracks very closely with PDO
 #SST seems to have aa negligable corelation 
@@ -71,15 +83,26 @@ set.seed(123)
 
 mod = depmix(list(CopeRich ~1, PDO~1),
              nstates = 2, 
-             transition = ~1, # the =~ means no covariates in transition?
+             transition = ~PDO, # the =~ means no covariates in transition?
              family = list(gaussian(),gaussian()),
              data=dat)
              
+#Nstates=how many possible states does the system have 
+#Transition= how quickly you jump between states
+  # 1= instantanious (one time step)
+  #0.5 = slower -- slope of the transition
+  #Put in PDO for the transition, the data from PDO is mediating the transition
+  #between copepod richness state 
+
+#AIC more states = better 
+  #need a biological rational for the number of states you have 
+
+
 fitmod = fit(mod)
 
 summary(fitmod)
 
-fit_pars<-getpars(fitmod)
+fit_pars<-getpars(fitmod) #understand these parameters more.
 
 init_states1<-fit_pars[1:2]
 mat1<-matrix(fit_pars[3:6],2,2,byrow=TRUE)
@@ -89,17 +112,18 @@ plot(ts(posterior(fitmod, type="smoothing")[,1], start=c(2003,5), deltat=1/12),y
      frame=FALSE)
 
 
-
 # Looking for best model
 ######################################################
 
+#Two state Assumptions
 
-multimod1 = depmix(list(CopeRich ~1, PDO~1),
+multimod2 = depmix(list(CopeRich ~1, PDO~1),
              nstates = 2, 
+             transition = ~PDO, 
              family = list(gaussian(),gaussian()),
              data=dat)
 
-fitmod = fit(multimod1)
+fitmod = fit(multimod2)
 
 iter <-100
 seeds<-sample(100:1000, size = iter, replace = F)
@@ -107,7 +131,7 @@ best <- 1e10
 best_model <- NA
 for(i in 1:iter){
   set.seed(seeds[i])
-  fitmod <- fit(multimod1)
+  fitmod <- fit(multimod2)
   if(AIC(fitmod)< best){
     best_model <- fitmod
     best <- AIC(fitmod)
@@ -125,10 +149,49 @@ plot(ts(posterior(best_model, type="smoothing")[,1], start=c(2003,5), deltat=1/1
      main="Posterior probability of state 1 (copepods good?).",
      frame=FALSE)
 
+#===================================
+#Three state Assumptions
+#===================================
+
+multimod3 = depmix(list(CopeRich ~1, PDO~1),
+                   nstates = 3, 
+                   transition = ~PDO, 
+                   family = list(gaussian(),gaussian()),
+                   data=dat)
+
+fitmod3 = fit(multimod3)
+
+iter <-100
+seeds<-sample(100:1000, size = iter, replace = F)
+best3 <- 1e10
+best_model <- NA
+for(i in 1:iter){
+  set.seed(seeds[i])
+  fitmod <- fit(multimod3)
+  if(AIC(fitmod3)< best3){
+    best_model <- fitmod3
+    best <- AIC(fitmod3)
+  }
+}
+
+summary(best_model)
+
+test<-getpars(best_model)
+
+init_states1<-test[1:2]
+mat_test<-matrix(test[3:6],2,2,byrow=TRUE)
+
+plot(ts(posterior(best_model, type="smoothing")[,1], start=c(2003,5), deltat=1/12),ylab="probability",
+     main="Posterior probability of state 1 (copepods good?).",
+     frame=FALSE)
+
+
 #Plot fits to data? 
 
 
-
+#States overlayed on data (plots from above)
+#Diagnostics 
+#comparative tables for 2 v 3 states 
 
 
 
