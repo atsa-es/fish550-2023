07233139bb85e2cfbcbf7cc1e8142418d335e4e5
shipmadison <shipmadison@gmail.com>
2023-05-08 11:31:50 -0700

    Update MHS_exploratory.R

--- Lab-4/Team-3/MHS_exploratory.R
+++ Lab-4/Team-3/MHS_exploratory.R
@@ -34,141 +34,105 @@ print(dat)
 
 library(hmmTMB)
 
-fitmod1 <- depmix(dat=surv_dat, 
-                  logit.s~1,
-                  nstates = 2,
-                  transition = ~ 1,
-                  family = gaussian())
+
+#exploring the data more 
+#rule 1 plot your data 
+
+# Plotting
+ggplot(dat, aes(x = years)) +
+  geom_line(aes(y = PDO, color = "PDO"), size = 1) +
+  geom_line(aes(y = SST, color = "SST"), size = 1) +
+  geom_line(aes(y = CopeRich, color = "CopeRich"), size = 1) +
+  #geom_line(aes(y = FishInx, color = "FishInx"), size = 1) +
+  scale_color_manual(values = c(PDO = "blue", SST = "red", CopeRich = "green", FishInx = "purple")) +
+  labs(x = "Year", y = "Value", color = "Variable") +
+  theme_minimal()
+
+#CopeRichness tracks very closely with PDO
+#SST seems to have aa negligable corelation 
+
+ggplot(dat, aes(x = PDO, y = CopeRich, color = CopeRich < 0)) +
+  geom_point() +
+  geom_line(data = data.frame(PDO, CopeRich), aes(x = PDO, y = CopeRich)) +
+  geom_hline(yintercept = 0, linetype = "dotted") +
+  geom_vline(xintercept = 0, linetype = "dotted") +
+  scale_color_manual(values = c("blue", "red"), labels = c("Above 0", "Below 0")) +
+  labs(x = "PDO", y = "CopeRich") +
+  theme_minimal()
+  
+
+#fish index all over the place, let's not use it. 
 
 
 #Playing around 
 set.seed(123)
 
-mod = depmix(list(CopeRich ~1, PDO~1, SST~1),
+#Copepods with PDO
+
+mod = depmix(list(CopeRich ~1, PDO~1),
              nstates = 2, 
-             family = list(gaussian(),gaussian(),gaussian()),
+             transition = ~1, # the =~ means no covariates in transition?
+             family = list(gaussian(),gaussian()),
              data=dat)
              
 fitmod = fit(mod)
 
 summary(fitmod)
 
-#Copepod richness as related to PDO 
-mod_PDO = depmix(CopeRich ~ 1 + PDO, 
-                  nstates = 2, 
-                  transition = ~ 1, # the =~ means no covariates in transition
-                  family = , #defaults to gaussian (response variable)
-                  data=dat)
+fit_pars<-getpars(fitmod)
 
-fitmod_PDO = fit(mod_PDO)
+init_states1<-fit_pars[1:2]
+mat1<-matrix(fit_pars[3:6],2,2,byrow=TRUE)
 
-summary(fitmod_PDO)
-test_PDO<-getpars(fitmod_PDO)
-
-init_states1<-test_PDO[1:2]
-mat1<-matrix(test_PDO[3:6],2,2,byrow=TRUE)
-
-plot(ts(posterior(fitmod_PDO, type="smoothing")[,1], start=c(2003,5), deltat=1/12),ylab="probability",
+plot(ts(posterior(fitmod, type="smoothing")[,1], start=c(2003,5), deltat=1/12),ylab="probability",
      main="Posterior probability of state 1 (copepods good?).",
      frame=FALSE)
 
 
-#More likely to stay in state one by a little bit
+
+# Looking for best model
+######################################################
 
 
-#Copepod richness as realted to SST 
-mod_SST = depmix(CopeRich ~ 1 + SST, 
-                  nstates = 2, 
-                  transition = ~ 1, # the =~ means no covariates in transition
-                  family = , #defaults to gaussian (response variable)
-                  data=dat)
-
-fitmod_SST = fit(mod_SST)
-
-summary(fitmod_SST)
-test_SST<-getpars(fitmod_SST)
-
-init_states2<-test_SST[1:2]
-mat2<-matrix(test_SST[3:6],2,2,byrow=TRUE)
-
-plot(ts(posterior(fitmod_SST, type="smoothing")[,1], start=c(2019,2), deltat=1/12),ylab="probability",
-     main="Posterior probability of state 1 (copepods good?).",
-     frame=FALSE)
-
-#State 2 is more stable than state 1 as it pertains to copepod richness 
-#states are being informed by SST? 
-
-
-#Play with Transition assumptions 
-
-#Copepod richness as related to PDO 
-mod_PDO_test = depmix(CopeRich ~ 1, 
-                 nstates = 2, 
-                 transition = ~PDO, # the =~ means no covariates in transition
-                 family = , #defaults to gaussian (response variable)
-                 data=dat)
-
-fitmod_PDO_test = fit(mod_PDO_test)
-
-summary(fitmod_PDO_test)
-test_PDO2<-getpars(fitmod_PDO_test)
-
-init_states1<-test_PDO2[1:2]
-mat_PDO_test<-matrix(test_PDO2[3:6],2,2,byrow=TRUE)
-
-plot(ts(posterior(fitmod_PDO_test, type="smoothing")[,1], start=c(2003,5), deltat=1/12),ylab="probability",
-     main="Posterior probability of state 1 (copepods good?).",
-     frame=FALSE)
-
-
-# Looking at inputs as a list 
-
-set.seed(123)
-
-multimod1 = depmix(list(CopeRich ~1, PDO~1, SST~1),
-             nstates = 2, 
-             family = list(gaussian(),gaussian(),gaussian()),
-             data=dat)
-
-fitmultimod = fit(multimod1)
-
-summary(fitmultimod)
-
-
-
-
-
-
-
-
-
-
-
-mod_test = depmix(CopeRich ~ 1
-                 nstates = 2, 
-                 transition = SST, # the =~ means no covariates in transition
-                 family = , #defaults to gaussian (response variable)
-                 data=dat)
-
-fitmod_SST = fit(mod_SST)
-
-summary(fitmod_SST)
-test_SST<-getpars(fitmod_SST)
-
-init_states2<-test_SST[1:2]
-mat2<-matrix(test_SST[3:6],2,2,byrow=TRUE)
-
-
-
-#Extreme example?
-mod = depmix(list(SST ~1, PDO ~ 1), 
+multimod1 = depmix(list(CopeRich ~1, PDO~1),
              nstates = 2, 
              family = list(gaussian(),gaussian()),
-             data=calcofi)
-fitmod = fit(mod)
+             data=dat)
+
+fitmod = fit(multimod1)
+
+iter <-100
+seeds<-sample(100:1000, size = iter, replace = F)
+best <- 1e10
+best_model <- NA
+for(i in 1:iter){
+  set.seed(seeds[i])
+  fitmod <- fit(multimod1)
+  if(AIC(fitmod)< best){
+    best_model <- fitmod
+    best <- AIC(fitmod)
+  }
+}
+
+summary(best_model)
+
+test<-getpars(best_model)
+
+init_states1<-test[1:2]
+mat_test<-matrix(test[3:6],2,2,byrow=TRUE)
+
+plot(ts(posterior(best_model, type="smoothing")[,1], start=c(2003,5), deltat=1/12),ylab="probability",
+     main="Posterior probability of state 1 (copepods good?).",
+     frame=FALSE)
+
+#Plot fits to data? 
 
 
-#pick some plankton, pick some drivers, 
+
+
+
+
+
 
 
 
