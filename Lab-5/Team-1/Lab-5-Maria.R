046a787ed00027fe6443f9371dad2ebc00b2e2f6
maria-kuruvilla <maria75799@gmail.com>
2023-05-19 17:12:00 -0700

    added pdo_w as covariate

--- Lab-5/Team-1/Lab-5-Maria.R
+++ Lab-5/Team-1/Lab-5-Maria.R
@@ -49,4 +49,42 @@ inits_list2 <- list(x0 = matrix(c(0,0,0), nrow = m))
 mod_list2 <- list(B = B, U = U, Q = Q, Z = Z, A = A, R = R)
 cont_list <- list(maxit=10000)
 # run model
-m3 <- MARSS(ratio, mod_list2, inits = inits_list2, control = cont_list)
+m3 <- MARSS(ratio, mod_list2, inits = inits_list2, control = cont_list, method = "BFGS")
+
+
+
+
+######################
+#                    #
+#      PART 2        #
+#                    #
+######################
+
+#first model we assume there is no density dependence
+#we model the underlying states of alpha (brood-year productivity) and beta
+#beta is prevented from changing.
+spawn_z <- matrix(scale(dat[2,]),nrow=1)  #z score spawners
+spawn_z <- spawn_z[,-c(1:4)]              #remove the NAs
+spawn <- matrix(dat[2,-c(1:4)],nrow=1)    #not sure if we should scale this or not
+ratio <- SR_data$y[-c(1:4)]               #remove responses that corresponded to Spawner = NA
+# z-score the predictor variable
+m <- 2                     #number of parameters in process 
+TT <- length(spawn_z)      #number of data points
+
+B <-  matrix(1)                         #"identity"
+U <-  matrix(0, nrow = 1, ncol = 1)   #"zero"
+Q <- matrix("q")  # to have characters and numbers in same matrix use a list
+Z <- matrix(1) # array(NA, c(1, m, TT))  ## NxMxT; empty for now
+# Z[1,1,] <- rep(1, TT)        ## Nx1; 1's for intercept
+# Z[1,2,] <- spawn_z             ## Nx1; predictor variable 
+A = matrix(0)            #"zero"
+R <-  matrix("r")
+d <- spawn
+D <- matrix("beta")
+
+inits_list <- list(x0 = matrix(c(0), nrow = 1))     
+mod_list <- list(B = B, U = U, Q = Q, Z = Z, A = A, R = R, d = d, D = D)
+
+## fit the model with MARSS
+m2 <- MARSS(ratio, mod_list, inits = inits_list)
+
