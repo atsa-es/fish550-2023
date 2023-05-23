f75106236765395ad33fa3114ae1259a82d9a0ca
Dylan Hubl <hubld@uw.edu>
2023-05-23 10:00:07 -0700

    added

--- Lab-5/Team-1/DH_explore.R
+++ Lab-5/Team-1/DH_explore.R
@@ -68,7 +68,7 @@ R <-  matrix("r")
 inits_list <- list(x0 = matrix(c(0, 0), nrow = m))     
 mod_list <- list(B = B, U = U, Q = Q, Z = Z, A = A, R = R)
 
-
+#this model uses beta/spawners as a covariate rather than a time stable variable in the z matrix
 mod3 <- list(
   Z=matrix(1,1),
   Q=matrix("q",1),
@@ -81,10 +81,10 @@ mod3 <- list(
 )
 init=list(x0=matrix(0,1))
 m3 <- MARSS(ratio,mod3,init)
-beta3 <- as.numeric(m3$coef)
+beta3 <- as.numeric(m3$coef)  
 
 plot(dat[5,]~dat[2,])
-## fit the model with MARSS
+## fit the model with MARSS this model uses beta/spawner as time invarying in the z matrix
 m2 <- MARSS(ratio, mod_list, inits = inits_list)
 
 hist(spawn)
@@ -126,13 +126,13 @@ autoplot.marssMLE(m2)
 #  Part 3   #
 #############
 
-#first model we assume there is no density dependence
+
 #we model the underlying states of alpha (brood-year productivity) and beta
-#beta is prevented from changing.
+
 #spawn_z <- matrix(scale(dat[2,]),nrow=1)  #z score spawners
 #spawn_z <- spawn_z[,-c(1:4)]              #remove the NAs
 pdo_s <- matrix(dat[3,-c(1:4)], nrow=1)
-#spawn <- matrix(dat[2,-c(1:4)],nrow=1)    #not sure if we should scale this or not
+spawn <- matrix(dat[2,-c(1:4)],nrow=1)    #not sure if we should scale this or not
             
 # z-score the predictor variable
 m <- 3                     #number of parameters in process 
@@ -141,7 +141,7 @@ TT <- length(spawn)      #number of data points
 B <-  diag(m)                         #"identity"
 U <-  matrix(0, nrow = m, ncol = 1)   #"zero"
 Q <- matrix(list(0, 0,0,              #we no longer want alpha to vary
-                 0, "q.beta",0,
+                 0, 0,0,              #hold beta steady as well
                  0,0,"q.pdo_s"), nrow = 3, byrow = TRUE)  # to have characters and numbers in same matrix use a list
 Z <- array(NA, c(1, m, TT))  ## NxMxT; empty for now
 Z[1,1,] <- rep(1, TT)        ## Nx1; 1's for intercept
@@ -154,14 +154,14 @@ inits_list2 <- list(x0 = matrix(c(0,0,0), nrow = m))
 mod_list2 <- list(B = B, U = U, Q = Q, Z = Z, A = A, R = R)
 cont_list <- list(maxit=10000)
 # run model
-m3 <- MARSS(ratio, mod_list2, inits = inits_list2)
+m4 <- MARSS(ratio, mod_list2, inits = inits_list2)
 
-alpha3 <- as.numeric(m3$states[1,])  #static number
-beta3 <- as.numeric(m3$states[2,])
-pdo_s <- as.numeric(m3$states[3,])
+alpha3 <- as.numeric(m4$states[1,])  #static number
+beta3 <- as.numeric(m4$states[2,])   #should be static as well
+pdo_s <- as.numeric(m4$states[3,])
 
-beta.se3 <- as.numeric(m3$states.se[2,])
-pdo_s.se3 <- as.numeric(m3$states.se[3,])
+beta.se3 <- as.numeric(m4$states.se[2,])
+pdo_s.se3 <- as.numeric(m4$states.se[3,])
 
 
 #from first model
@@ -178,15 +178,46 @@ lines(alpha3~SR_data$brood_year[-c(1:4)],type='l', col="blue")
 
 m1$states[-c(1:4)] #alpha from model 1 trimmed to same length as model 2 for comparison
 m2$states[1,]      #alpha from model 2
-m3$states[1,]     #alpha from model 3
+m4$states[1,]     #alpha from model 3
 m2$states[2,]     # beta from model 2
-m3$states[2,]     #beta from model 3
-m3$states[3,]    #estimate of pdo_s coeficient was allowed to vary in Q but was estimated as static
+m4$states[2,]     #beta from model 3
+m4$states[3,]    #estimate of pdo_s coeficient was allowed to vary in Q but was estimated as static
 
 #AIC value
 m1$AICc
 m2$AICc  #AICc is within a single point.
-m3$AICc  # getting worse
+m4$AICc  # getting worse
 
-autoplot.marssMLE(m3)
+autoplot.marssMLE(m4)
 #more autocorrelations in acf plot
+
+#########model with PDO winter
+pdo_w <- matrix(dat[4,-c(1:4)], nrow=1)
+spawn <- matrix(dat[2,-c(1:4)],nrow=1)    #not sure if we should scale this or not
+
+# z-score the predictor variable
+m <- 3                     #number of parameters in process 
+TT <- length(spawn)      #number of data points
+
+B <-  diag(m)                         #"identity"
+U <-  matrix(0, nrow = m, ncol = 1)   #"zero"
+Q <- matrix(list(0, 0,0,              #we no longer want alpha to vary
+                 0, 0,0,              #hold beta steady as well
+                 0,0,"q.pdo_w"), nrow = 3, byrow = TRUE)  # to have characters and numbers in same matrix use a list
+Z <- array(NA, c(1, m, TT))  ## NxMxT; empty for now
+Z[1,1,] <- rep(1, TT)        ## Nx1; 1's for intercept
+Z[1,2,] <- spawn           ## Nx1; predictor variable spawners
+Z[1,3,] <-  pdo_w            ## Nx1; predictor PDO winter
+A = matrix(0)                #"zero"
+R <-  matrix("r")
+
+inits_list2 <- list(x0 = matrix(c(0,0,0), nrow = m))     
+mod_list2 <- list(B = B, U = U, Q = Q, Z = Z, A = A, R = R)
+
+# run model
+m5 <- MARSS(ratio, mod_list2, inits = inits_list2)
+
+m1$AICc  #model 1: time varying alpha only
+m2$AICc  #model 2: time varying alpha, time stable beta
+m4$AICc  #model 3: time stable alpha and beta, time varying PDO_summer
+m5$AICc  #model 4: time stable alpha and beta, time varying PDO_winter
